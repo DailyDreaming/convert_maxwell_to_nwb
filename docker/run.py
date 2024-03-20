884bb5ae955b35ebf6dcccc731dd289feae50095
Lon Blauvelt <lblauvel@ucsc.edu>
2024-03-20 11:16:42 -0700

    Update run.py

--- docker/run.py
+++ docker/run.py
@@ -2,11 +2,9 @@
 """
 Takes any supported (local) file or list of files as arguments and converts to NWB format.
 
-Output files are placed in /tmp/nwb/.
+To run, mount the directory containing your input file path(s), and the output should appear in the same directory:
 
-To run:
-
-  docker run -v /home/quokka/outputs:/tmp/nwb quay.io/ucsc_cgl/nwb-converter:0.1 python3 /bin/run.py your_maxwell_file.raw.h5
+  docker run -v /home/quokka/data:/home/quokka/data quay.io/ucsc_cgl/nwb-converter:0.1 python3 /bin/run.py /home/quokka/data/your_maxwell_file.raw.h5
 """
 import os
 import sys
